0d7525be5f50127fe5b1ee35b669c1548cd5af0a
Bugaj Patrycja <PB5060@ilot.local>
2018-06-29 08:35:55 +0200

    Initial commit

--- app/scripts/controllers/lots-o-bricks.js
+++ app/scripts/controllers/lots-o-bricks.js
@@ -8,36 +8,20 @@
  * Controller of the serviceQuizApp
  */
 angular.module('serviceQuizApp')
-  .controller('LotsOBricksCtrl', function () {
+  .controller('LotsOBricksCtrl', ['brickWarehouse', function (lotsStore) {
     this.name = 'Lots O Bricks';
-    this.redBricks = {
-      '1x1': {
-        quantity: 16,
-        price: 0.01
-      },
-      '2x6': {
-        quantity: 18,
-        price: 0.05
-      }
-    };
-    this.blueBricks = {
-      '2x2': {
-        quantity: 7,
-        price: 0.02
-      },
-      '2x4': {
-        quantity: 2,
-        price: 0.04
-      }
-    };
-    this.greenBricks = {
-      '2x4': {
-        quantity: 13,
-        price: 0.04
-      },
-      '2x8': {
-        quantity: 41,
-        price: 0.08
-      }
-    };
-  });
+
+    var vm = this;
+
+    lotsStore.getRedBricks().then(function(data){
+      vm.redBricks = data;
+    });
+
+    lotsStore.getBlueBricks().then(function(data){
+      vm.blueBricks = data;
+    });
+
+    lotsStore.getGreenBricks().then(function(data){
+      vm.greenBricks = data;
+    });
+  }]);
---
+++ app/scripts/services/brickwarehouse.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name serviceQuizApp.brickWarehouse
+ * @description
+ * # brickWarehouse
+ * Service in the serviceQuizApp.
+ */
+angular.module('serviceQuizApp')
+  .service('brickWarehouse', function () {
+    // this.getBricksABunch = function(){
+    // 	return $.get('/brick/bricksABunch.json');
+    // };
+    // this.getRedBricks = function(){
+    // 	return $.get('/brick/redBricks.json');
+    // };
+    // this.getBlueBricks = function(){
+    // 	return $.get('/brick/blueBricks.json');
+    // };
+    // this.getGreenBricks = function(){
+    // 	return $.get('/brick/greenBricks.json');
+    // };
+  });
